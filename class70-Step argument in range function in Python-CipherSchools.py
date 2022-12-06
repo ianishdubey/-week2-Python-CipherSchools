528046cec1277ae20720cd1b005bec3e22d3b654
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-06 13:54:30 +0530

    class70-Step argument in range function in Python-CipherSchools

---
+++ class70-Step argument in range function in Python-CipherSchools.py
@@ -0,0 +1,14 @@
+# step argument
+
+for i in range(0,11,2):
+    print(i)
+
+
+#  to print - 10,9,8
+
+for i in range(10,0,-1):
+    print(i)
+
+# another way
+for i in range(1,11,-1):
+    print(i)
