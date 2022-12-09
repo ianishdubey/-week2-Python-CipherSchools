5b38501de0346afc70ac8e4b69623b63c6bab9b7
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-09 23:02:42 +0530

    class95-Looping in list in Python-CipherSchools

---
+++ class95-Looping in list in Python-CipherSchools.py
@@ -0,0 +1,12 @@
+fruits = ['orange','apple','pear','banana','kiwi']
+
+# for fruits
+for fruit in fruits:
+    print(fruit)
+
+
+# while
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i+=1
