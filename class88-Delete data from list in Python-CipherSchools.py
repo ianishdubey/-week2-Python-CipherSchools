1ec0a7a47fde0e9696f12ae0e25985fcec66fadb
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-09 20:03:21 +0530

    class88-Delete data from list in Python-CipherSchools

---
+++ class88-Delete data from list in Python-CipherSchools.py
@@ -0,0 +1,18 @@
+# common methods to add data from list
+fruits = ['orange','apple','pear','banana','apple','kiwi']
+
+# pop method 
+fruits.pop()
+print(fruits)
+
+fruits.pop(1)
+print(fruits)
+
+# del
+del fruits[1]
+print(fruits)
+
+# remove
+fruits.remove('apple')
+
+print(fruits)
