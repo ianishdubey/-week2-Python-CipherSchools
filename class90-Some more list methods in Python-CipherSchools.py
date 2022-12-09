35e72c9c3dbb493fff5741cad8b31077b28fb993
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-09 20:05:32 +0530

    class90-Some more list methods in Python-CipherSchools

---
+++ class90-Some more list methods in Python-CipherSchools.py
@@ -0,0 +1,22 @@
+# count
+# sort method
+# sorted function
+# reverse
+# clear
+# copy
+fruits = ['orange','apple','pear','banana','kiwi']
+print(fruits.count('orange'))
+
+fruits.sort()
+print(fruits)
+
+numbers = [3,5,1,9,10]
+numbers.sort()
+print(sorted(numbers)) 
+
+# clear function
+# numbers.clear()
+# print(numbers)
+
+numbers_copy=numbers.copy()
+print(numbers_copy)
