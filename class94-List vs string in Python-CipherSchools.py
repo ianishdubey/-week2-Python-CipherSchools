9d2bb9606120329a051c85ea67d93fa9a7ba3a2e
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-09 23:02:09 +0530

    class94-List vs string in Python-CipherSchools

---
+++ class94-List vs string in Python-CipherSchools.py
@@ -0,0 +1,15 @@
+# Lists vs Strings
+
+# Strings are immutable
+# Lists are mutable
+
+s = "string"
+t = s.title()
+print(t)
+
+l = ['word 1','word 2','word 3']
+l.pop()
+print(l)
+
+l.append("word3")
+print(l)
