8081bb17fa7b11d143939f384727aab79e485ee0
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-09 20:05:56 +0530

    class91-is vs equals in Python-CipherSchools

---
+++ class91-is vs equals in Python-CipherSchools.py
@@ -0,0 +1,8 @@
+# compare lists
+# ==,is
+fruits1 = ['orange','apple','pear']
+fruits3 = ['orange','apple','pear']
+fruits2 = ['banana','kiwi','apple','banana']
+print(fruits1 == fruits2) #values are different
+print(fruits1 == fruits3) #values are same
+print(fruits1 is fruits3) #false
