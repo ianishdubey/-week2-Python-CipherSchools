90270059beb054b4656a3dda5667c4e4f39be431
Anish Dubey <117798074+ianishdubey@users.noreply.github.com>
2022-12-10 18:02:39 +0530

    class111-Chapter 5 Summary in Python-CipherSchools

---
+++ class111-Chapter 5 Summary in Python-CipherSchools.py
@@ -0,0 +1,12 @@
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError as err:
+        # print("you cannot divide a number by zero")
+        print(err)
+    except TypeError as err:
+        print("numbers must be int or floats")
+    except:
+        print("unexpected error")
+
+print(divide(16,'2'))
